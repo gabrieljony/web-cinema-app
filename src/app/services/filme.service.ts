572764b45893a9861a785764f92107cfc6bbcfa2
Gabriel Jony Borges Silva <gabjony@gmail.com>
2019-05-06 20:43:05 -0300

    feature/gabriel-melhoria-service-filme

--- src/app/services/filme.service.ts
+++ src/app/services/filme.service.ts
@@ -13,38 +13,25 @@ export class FilmeService {
 
   constructor(private http: HttpClient) { }
 
-  getBuscar(query: string) {
-    const url = `https://api.themoviedb.org/3${query}&api_key=${
-      this.apikey
-      }&language=pt-br&callback=JSONP_CALLBACK`;
-
-    return this.http.jsonp(url, '');
-  }
-
   getBuscarPorFilme(query: string) {
-    const url = `https://api.themoviedb.org/3${query}?api_key=${
-      this.apikey
-      }&language=pt-br&callback=JSONP_CALLBACK`;
+    const url = `${this.urlAPI}${query}?api_key=${this.apikey}&language=pt-br&callback=JSONP_CALLBACK`;
 
     return this.http.jsonp(url, '');
   }
 
   getDiscoverMovie() {
-    return this.getBuscar('/discover/movie?sort_by=popularity.desc').pipe(
-       map((data: any) => data.results)
-    );
+    return this.getBuscarPorFilme(`/discover/movie`)
+    .pipe(map((data: any) => data.results));
   }
 
   getSearchMovie(termino: string) {
-    return this.getBuscar(
-      `/search/movie?query=${termino}&sort_by=popularity.desc`
-    ).pipe(map((data: any) => data.results));
+    return this.getBuscarPorFilme(`/search/movie?query=${termino}&sort_by=popularity.desc`)
+    .pipe(map((data: any) => data.results));
   }
 
   getFilme(id: string) {
-    return this.getBuscarPorFilme(`/movie/${id}`).pipe(
-      map((data: any) => data)
-    );
+    return this.getBuscarPorFilme(`/movie/${id}`)
+    .pipe(map((data: any) => data));
   }
 
 }
