b02d5c2c09513cd6f9471af5999e45bc95150ff2
Gabriel Jony <gjbsilva@tjba.jus.br>
2019-05-06 14:37:04 -0300

    feature/gabriel-service

--- src/app/services/filme.service.ts
+++ src/app/services/filme.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmeService {
 
-  constructor() { }
+  private urlAPI: string = 'https://api.themoviedb.org/3';
+  private apikey: string = '67f2745bfbab836b3214ba02776d32ba';
+
+  constructor(private http: HttpClient) { }
+
+  getBuscar(query: string) {
+    const url = `${this.urlAPI}${query}&api_key=${
+      this.apikey
+      }&language=pt-br&callback=JSONP_CALLBACK`;
+
+    return this.http.jsonp(url, '');
+  }
+
+  getBuscarPorFilme(query: string) {
+    const url = `${this.urlAPI}${query}?api_key=${
+      this.apikey
+      }&language=pt-br&callback=JSONP_CALLBACK`;
+
+    return this.http.jsonp(url, '');
+  }
+
+  getDiscoverMovie() {
+    return this.getFilme('/discover/movie?sort_by=popularity.desc').pipe(
+       map((data: any) => data.results)
+    );
+  }
+
+  getSearchMovie(termino: string) {
+    return this.getFilme(
+      `/search/movie?query=${termino}&sort_by=popularity.desc`
+    ).pipe(map((data: any) => data.results));
+  }
+
+  getFilme(id: string) {
+    return this.getBuscarPorFilme(`/movie/${id}`).pipe(
+      map((data: any) => data)
+    );
+  }
+
 }
