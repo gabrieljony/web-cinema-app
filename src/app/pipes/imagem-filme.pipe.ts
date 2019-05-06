0729404f6078415abccf81e62c0146697d97fb37
Gabriel Jony <gjbsilva@tjba.jus.br>
2019-05-06 14:38:50 -0300

    feature/gabriel-pipe

--- src/app/pipes/imagem-filme.pipe.ts
+++ src/app/pipes/imagem-filme.pipe.ts
@@ -5,8 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ImagemFilmePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(filme: any): any {
+
+    let url: string = 'http://image.tmdb.org/t/p/w400';
+
+    if (filme.poster_path) {
+      return url + filme.poster_path;
+    } else {
+      if (filme.backdrop_path) {
+        return url + filme.backdrop_path;
+      } else {
+        return 'assets/img/noimage.png';
+      }
+    }
   }
 
 }
