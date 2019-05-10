9d8dedf0d47c46885c3d3d5d4cb26a9148b1ddb0
Gabriel Jony Borges Silva <gabjony@gmail.com>
2019-05-09 23:27:07 -0300

    feature/gabriel-correcao-paginacao

--- src/app/home-lista/home.component.ts
+++ src/app/home-lista/home.component.ts
@@ -9,37 +9,29 @@ import { FilmeService } from 'src/app/services/filme.service';
 export class HomeComponent {
 
   novosFilmes: Object[] = [];
-  loading: boolean;
   valorInput: string;
 
   constructor(private filmeService: FilmeService) {
 
-    this.loading = true;
-
     this.filmeService.getDiscoverMovie()
       .subscribe((data: any) => {
         this.novosFilmes = data;
-        this.loading = false;
       });
 
   }
 
   buscar(input: string) {
-    this.loading = true;
 
     this.filmeService.getSearchMovie(input)
       .subscribe((data: any) => {
         this.novosFilmes = data;
-        this.loading = false;
       });
   }
 
   onMudouPagina(event) {
-    this.loading = true;
     
     this.filmeService.getPage(event.novoValor).subscribe((data: any) => {
       this.novosFilmes = data;
-      this.loading = false;
     });
   }
 
